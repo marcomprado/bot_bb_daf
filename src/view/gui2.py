ad7ea287658ea859ea18f123acec32cdd03b0748
marco martinelli <marcomarti1000@gmail.com>
2025-09-03 14:53:25 -0300

    import corrections

--- src/view/gui2.py
+++ src/view/gui2.py
@@ -610,7 +610,7 @@ class GUI2:
         """Abre a pasta de arquivos FNDE no explorador"""
         try:
             # Caminho da pasta FNDE usando a mesma lógica do bot_fnde.py
-            from gui1 import obter_caminho_dados
+            from src.view.gui1 import obter_caminho_dados
             diretorio_base = obter_caminho_dados("arquivos_baixados")
             pasta_fnde = os.path.join(diretorio_base, "fnde")
             
